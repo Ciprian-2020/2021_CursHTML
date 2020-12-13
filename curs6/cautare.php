f91955efc50c046bbf887e022f354bb8cf73e935
Adrian Adiaconitei <adrian@hipo.ro>
2020-12-13 02:00:47 +0200

    curs6

---
+++ curs6/cautare.php
@@ -0,0 +1,13 @@
+<?php
+// echo "<pre>";
+// print_r($_POST);//print_r($_GET);
+
+
+$sir1="Popescu Mihaela";
+$sir2="Popescu Mihai";
+if(strcmp($sir1, $sir2))
+echo "Sirurile sunt diferite!";
+else
+echo "Sirurile sunt egale!";
+
+ ?>
